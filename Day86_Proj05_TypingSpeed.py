cfad343f2bbc7f5d987bfcae19c636ac8b0e3422
Thomas3Dsys <thomas3d@gmail.com>
2024-09-12 10:18:17 -0700

    add comments to code and add user feedback during typing

--- Day86_Proj05_TypingSpeed.py
+++ Day86_Proj05_TypingSpeed.py
@@ -11,42 +11,47 @@ cursor = 0 # character cursor
 
 print(f"Testing typing of {word_count} words")
 
-      
-
 def key_pressed(event):
     """ 
-        When the user types the next correct letter, increment the cursor. 
-        Next highlight the text up to the cursor position.
+        If the user is focus is set on the input text area, 
+        when the user types the next correct letter, increment the cursor. 
+        Next highlight the text up to the current cursor position.
         Start the timer when the first matching character is typed.
+        when a space is pressed , check if the correct word is found in the text area
+        if the word is completed correctly, increment the word counter and clear the text area
         Call the done method when the cursor is at the end of the sentence
         """
     global cursor, done, start, word_cursor, current_word
 
     if done_flag == True:
         return
-
     
     try:
         if win.focus_get()._name != "!text2" : 
             print("Type in the input box")
+            done_label.configure(text="Type in the input box.")
             return
     except: # not focus
         print("Type in the input box")
+        done_label.configure(text="Type in the input box.")
         return
 
     print(cursor)
     
     if(cursor == len(SENTENCE)-1):
         highlight_to(text,f"1.{len(SENTENCE)}")
+        done_label.configure(text="You are done!")
         done()
         return
     
     if(cursor == 1):
         print("Start Timer")
         start = time.time()
+        done_label.configure(text="Type the words as quickly as possible.")
     
     next_char = SENTENCE[cursor]
     if(event.char == next_char):
+        done_label.configure(text="Type the words as quickly as possible.")
         if event.char == " ":
             if current_word  == entry.get("1.0", END).replace(" ","").replace("\n", ""):
                 entry.delete("1.0", END)
@@ -58,7 +63,8 @@ def key_pressed(event):
         else:
             cursor += 1
             highlight_to(text,f"1.{cursor}")
-               
+    else:
+        done_label.configure(text="Delete any mis-typed chacter before continuing.")
         
 
         
@@ -88,21 +94,20 @@ def highlight_to(text, position):
 win=Tk()
 win.geometry("700x400")
 
+#Text Area Holding the sentence to type
 text=Text(win, width=80, height=6, font=('Calibri 24'), bg="#CCCCCC")
 text.insert(INSERT, SENTENCE)
-
 text.tag_configure("start", background="OliveDrab1", foreground="black")
 text.configure(state="disabled")
 text.pack()
 
+#The input area for user typing
 entry=Text(win, width=20, height=1, font=('Calibri 24'), bg="#dddddd")
-
 entry.pack(pady = 10)
 
-
-done_label=Label(win, font=('Calibri 14'), fg="#4444aa", text="Begin Typing to start the timer.")
+#Label to user
+done_label=Label(win, font=('Calibri 14'), fg="#4444aa", text="Click the input area and begin Typing to start.")
 done_label.pack()
-
 
 current_word = SENTENCE.split(" ")[0]
 win.bind("<Key>",key_pressed)#handles the user input
